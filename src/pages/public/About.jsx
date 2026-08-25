e4905dc8625ba473a053fc394a80f70c652d559a
Iqra-Mushtak <iqramushtaq2563@gmail.com>
2026-08-25 23:22:22 +0500

    react import update

--- src/pages/public/About.jsx
+++ src/pages/public/About.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './About.css';
 import logoImg from '../../assets/logo.png';
